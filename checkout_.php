e9f89fe403ce9ae753efdaf68623b542a688f1c5
davidSolarte1 <dsolarte2809@gmail.com>
2023-03-08 20:02:49 -0500

    implementacion paypal

---
+++ checkout_.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <script src="https://www.paypal.com/sdk/js?client-id=ATi5gpbweeYOeq3ohjaE6avVJez6G7HkTH8g0sO_TxpwVNQzxoNFZe9OP-PZnQ8E8ZvxmpPrbrzWR4Kg"></script>
+</head>
+<body>
+    <!--
+    <div id="paypal-button-container">
+
+    </div>
+
+    <script>
+        paypal.Buttons({
+            style:{
+                color: 'blue',
+                shape: 'pill',
+                label: 'pay'
+            },
+            createOrder: function(data, actions){
+                return actions.order.create({
+                    purchase_units: [{
+                        amount: {
+                            value: 2000
+                        }
+                    }]
+                })
+            },
+
+            onCancel: function(data){
+                alert("Pago Cancelado"):
+                console.log(data)
+            }
+        }).render('#paypal-button-container');
+    </script>*/ -->
+</body>
+</html>
