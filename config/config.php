97c704b7046e1cd40b2046ce7bf4b56f5d6482d6
davidSolarte1 <dsolarte2809@gmail.com>
2023-03-10 13:58:21 -0500

    pagos con bases de datos

--- config/config.php
+++ config/config.php
@@ -1,5 +1,7 @@
 <?php
 
+define("CLIENT_ID", "Aa7DJssZI2_UHgMjnvsqQlqmsRmfRvTsANfb6GVIBJfIG4if1U1_Z2mdNMjqiQxKG5LnXkh_J071oFbr");
+define("CURRENCY", "USD");
 define("KEY_TOKEN", "WazaSA.BEL-12");
 define("MONEDA", "$");
 
---
+++ pago.php
@@ -0,0 +1,202 @@
+<?php
+
+require 'config/config.php';
+require 'config/database.php';
+$db = new Database();
+$con = $db->conectar();
+
+$productos = isset($_SESSION['carrito']['productos']) ? $_SESSION['carrito']['productos'] : null;
+
+//print_r($_SESSION);
+
+$lista_carrito = array();
+
+if($productos != null){
+    foreach($productos as $clave => $cantidad){
+        $sql = $con->prepare("SELECT id,nombre,precio,descuento, $cantidad AS cantidad FROM productos WHERE id=? AND activo=1");
+        $sql->execute([$clave]);
+        $lista_carrito[] = $sql->fetch(PDO::FETCH_ASSOC);
+
+    }
+}else{
+    header("Location: index.php");
+    exit;
+}
+
+
+
+//session_destroy();
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>La Bellaquea</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+    <header>
+        <div class="navbar navbar-expand-lg navbar-dark bg-dark ">
+            <div class="container">
+                <a href="#" class="navbar-brand ">
+                    <strong>Tienda</strong>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+            <div class="collapse navbar-collapse" id="navbarHeader">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link active">Catalogo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Contacto</a>
+                    </li>
+                </ul>
+                <a href="checkout.php" class="btn btn-primary">
+                    Carrito <span id="num_cart" class="badge bg-secondary"><?php echo $num_cart; ?></span>
+                </a>
+            </div>
+
+            </div>
+        </div>
+    </header>
+    <!--Contenido-->
+    <main>
+        <div class="container">
+
+            <div class="row">
+                <div class="col-6">
+                    <h4>Detalles de pago</h4>
+                    <div id="paypal-button-container"></div>
+                </div>
+
+                <div class="col-6">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Producto</th>
+                                    <th>Subtotal</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php if($lista_carrito == null){
+                                    echo '<tr><td colspan="5" class="text-center"><b>Lista vacia</b></td></tr>';
+                                }else{
+        
+                                    $total = 0;
+                                    foreach($lista_carrito as $producto){
+                                        $_id = $producto['id'];
+                                        $nombre = $producto['nombre'];
+                                        $precio = $producto['precio'];
+                                        $descuento = $producto['descuento'];
+                                        $cantidad = $producto['cantidad'];
+                                        $precio_desc = $precio - (($precio*$descuento)/100);
+                                        $subtotal = $cantidad * $precio_desc;
+                                        $total += $subtotal;
+                                        ?>
+                                    
+                                <tr>
+                                    <td><?php echo $nombre; ?></td>
+                                    <td>
+                                        <div id="subtotal_<?php echo $_id;?>" name="subtotal[]"><?php echo MONEDA . number_format($subtotal, 0,',','.'); ?></div>
+                                    </td>                            
+                                </tr>
+                                <?php } ?>
+        
+                                <tr>
+                                    
+                                    <td colspan="2">
+                                        <p class="h3 text-end" id="total"><?php echo MONEDA . number_format($total, 0,',','.'); ?></p>
+                                    </td>
+                                </tr>
+                            </tbody>
+                            <?php } ?> 
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+    <script src="https://www.paypal.com/sdk/js?client-id=<?php echo CLIENT_ID;?>&currency=<?php echo CURRENCY?>" data-sdk-integration-source="button-factory"></script>
+
+    <script src="https://www.paypal.com/sdk/js?client-id=Aa7DJssZI2_UHgMjnvsqQlqmsRmfRvTsANfb6GVIBJfIG4if1U1_Z2mdNMjqiQxKG5LnXkh_J071oFbr&currency=USD" data-sdk-integration-source="button-factory"></script>
+  <script>
+    function initPayPalButton() {
+      paypal.Buttons({
+        style: {
+          shape: 'pill',
+          color: 'blue',
+          layout: 'vertical',
+          label: 'pay',
+          
+        },
+
+        createOrder: function(data, actions) {
+          return actions.order.create({
+            purchase_units: [{
+                amount:{
+                    value : <?php echo $total;?>
+                }
+            }]
+          });
+        },
+
+        onCancel: function(data){
+            alert("Pago Cancelado")
+        },
+
+        onApprove: function(data, actions) {
+            let URL = 'clases/captura.php'
+          return actions.order.capture().then(function(detalles) {
+            console.log(detalles);
+            let url = 'clases/captura.php'
+            return fetch(url,{
+                method: 'post',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+
+                    detalles: detalles
+                })
+            })
+            /*
+            // Full available details
+            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+
+            // Show a success message within this page, e.g.
+            const element = document.getElementById('paypal-button-container');
+            element.innerHTML = '';
+            element.innerHTML = '<h3>Thank you for your payment!</h3>';
+
+            // Or go to another URL:  actions.redirect('thank_you.html');
+            */
+          });
+        },
+
+        onError: function(err) {
+          console.log(err);
+        }
+      }).render('#paypal-button-container');
+    }
+    initPayPalButton();
+  </script>
+
+
+</body>
+</html>
